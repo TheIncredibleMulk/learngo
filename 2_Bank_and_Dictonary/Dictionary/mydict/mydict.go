e74c1e4b7727e5bda58138a6f1980ec93c26a9cc
TheIncredibleMulk <andrew.mulkey@gmail.com>
2020-11-28 20:00:53 -0500

    2.5 add method, make my head hurt

--- 2_Bank_and_Dictonary/Dictionary/mydict/mydict.go
+++ 2_Bank_and_Dictonary/Dictionary/mydict/mydict.go
@@ -8,6 +8,7 @@ import (
 type Dictionary map[string]string
 
 var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That word already exists")
 
 // Search Dictionary word
 func (d Dictionary) Search(word string) (string, error) {
@@ -17,4 +18,16 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 	return "", errNotFound
 
+}
+
+// Add a new word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
 }
