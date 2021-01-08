ad0e55a69edea13a17776cd966b5f7456ded1a52
TheIncredibleMulk <andrew.mulkey@gmail.com>
2021-01-07 23:28:28 -0500

    before looping through channels

--- 3_URLChecker/concurrency/main.go
+++ 3_URLChecker/concurrency/main.go
@@ -6,14 +6,28 @@ import (
 )
 
 func main() {
-	go sexyCount("nico")
-	go sexyCount("flynn")
-	time.Sleep(time.Second * 5)
+	channel := make(chan string)
+	people := [2]string{"nico", "flynn"}
+	for _, person := range people {
+		go isSexy(person, channel)
+	}
+	fmt.Println("Waiting for messages")
+	resultOne := <-channel
+	resultTwo := <-channel
+	fmt.Println("Recieved this message: ", resultOne)
+	fmt.Println("Recieved this message: ", resultTwo)
 }
 
+func isSexy(person string, channel chan string) {
+	time.Sleep(time.Second * 10)
+	channel <- person + " is sexy"
+}
+
+/*
 func sexyCount(person string) {
 	for i := 0; i < 10; i++ {
 		fmt.Println(person, "is sexy", i)
 		time.Sleep(time.Second)
 	}
 }
+*/
