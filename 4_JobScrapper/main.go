cad929d71315578c6fa910a6a0efafd8793c47ec
TheIncredibleMulk <andrew.mulkey@gmail.com>
2021-01-13 15:37:08 -0500

    paginaton added and more to check multiple pages

--- 4_JobScrapper/main.go
+++ 4_JobScrapper/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/Puerkitobio/goquery"
 )
 
-var baseURL string = "https://www.indeed.com/jobs?q=python&start=20"
+var baseURL string = "https://www.indeed.com/jobs?q=python&limit=50&start="
 
 func main() {
-	getPages()
+	totalPages := getPages()
+
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
+	}
+}
+
+func getPage(page int) {
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("Requesting", pageURL)
 }
 
 func getPages() int {
+	pages := 0
 	res, err := http.Get(baseURL)
 	checkErr(err)
 	checkCode(res)
@@ -21,9 +33,11 @@ func getPages() int {
 	defer res.Body.Close()
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
-	doc.Find(".pagination")
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
 
-	return 0
+	return pages
 }
 
 func checkErr(err error) {
