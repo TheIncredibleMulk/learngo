b221468c91c4b8ef28bfa170ba643d6f56698cc8
TheIncredibleMulk <andrew.mulkey@gmail.com>
2021-01-13 09:49:09 -0500

    end of 4.0, added a way to scrape webpages using goquery

--- 4_JobScrapper/main.go
+++ 4_JobScrapper/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/Puerkitobio/goquery"
+)
+
+var baseURL string = "https://www.indeed.com/jobs?q=python&start=20"
+
+func main() {
+	getPages()
+}
+
+func getPages() int {
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+	doc.Find(".pagination")
+
+	return 0
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Responce Failed with Status:", res.StatusCode)
+	}
 }
