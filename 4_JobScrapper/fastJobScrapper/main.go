cbb8ee7d323b9aee3dfba8e29ea75be95f01cfe1
TheIncredibleMulk <andrew.mulkey@gmail.com>
2021-01-13 20:30:24 -0500

    completed fastJobScrapper 4.7 with Nico, need to complete making the write function faster with goroutines

--- 4_JobScrapper/fastJobScrapper/main.go
+++ 4_JobScrapper/fastJobScrapper/main.go
@@ -25,37 +25,21 @@ var baseURL string = "https://www.indeed.com/jobs?q=python&limit=50&start="
 func main() {
 	var jobs []extractedJob
 	totalPages := getPages()
+	c := make(chan []extractedJob)
 
 	for i := 0; i < totalPages; i++ {
-		extractedJobs := getPage(i)
+		go getPage(i, c)
+
+	}
+	for i := 0; i < totalPages; i++ {
+		extractedJobs := <-c
 		jobs = append(jobs, extractedJobs...)
 	}
-
 	writeJobs(jobs)
 	fmt.Println("Done, extraced", len(jobs))
 }
 
-func writeJobs(jobs []extractedJob) {
-	file, err := os.Create("jobs.csv")
-	checkErr(err)
-
-	w := csv.NewWriter(file)
-	defer w.Flush()
-
-	headers := []string{"Link", "Title", "Location", "Salary", "Summary"}
-
-	wErr := w.Write(headers)
-	checkErr(wErr)
-
-	for _, job := range jobs {
-		jobSlice := []string{"https://indeed.com/viewjob?k=" + job.id, job.title, job.location, job.salary, job.summary}
-		jwErr := w.Write(jobSlice)
-		checkErr(jwErr)
-
-	}
-}
-
-func getPage(page int) []extractedJob {
+func getPage(page int, mainC chan<- []extractedJob) {
 	var jobs []extractedJob
 	c := make(chan extractedJob)
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
@@ -73,10 +57,13 @@ func getPage(page int) []extractedJob {
 
 	searchCards.Each(func(i int, card *goquery.Selection) {
 		go extractJob(card, c)
-		// jobs = append(jobs, job)
-	})
 
-	return jobs
+	})
+	for i := 0; i < searchCards.Length(); i++ {
+		job := <-c
+		jobs = append(jobs, job)
+	}
+	mainC <- jobs
 }
 
 func extractJob(card *goquery.Selection, c chan<- extractedJob) {
@@ -114,6 +101,26 @@ func getPages() int {
 func checkErr(err error) {
 	if err != nil {
 		log.Fatalln(err)
+	}
+}
+
+func writeJobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"Link", "Title", "Location", "Salary", "Summary"}
+
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{"https://indeed.com/viewjob?k=" + job.id, job.title, job.location, job.salary, job.summary}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
+
 	}
 }
 
